ea9f90cec3c49f9c10e9f05792deda42c26dbabb
ALFABURST artemis <griffin.foster@gmail.com>
2017-01-23 11:35:39 -0400

    updated where to store log files, useful to get RFI bandpass of both beams on a node

--- Scripts/frb_abc1.sh
+++ Scripts/frb_abc1.sh
@@ -3,15 +3,20 @@ mkdir /data/Survey/Data
 /home/artemis/Survey/Scripts/killobs
 /home/artemis/Survey/Scripts/killobs
 /home/artemis/Survey/Scripts/killobs
-numactl -C 0-5 -l ABPipeline --config=/home/artemis/Survey/Config/Beam2_client.xml &> /data/Survey/Log/pipeline0.log &
+cd /data/Survey/Log/beam2
+numactl -C 0-5 -l ABPipeline --config=/home/artemis/Survey/Config/Beam2_client.xml &> /data/Survey/Log/beam2/pipeline0.log &
 pidp0=$!
-numactl -C 6-11 -l ABPipeline --config=/home/artemis/Survey/Config/Beam3_client.xml &> /data/Survey/Log/pipeline1.log &
+cd /data/Survey/Log/beam3
+numactl -C 6-11 -l ABPipeline --config=/home/artemis/Survey/Config/Beam3_client.xml &> /data/Survey/Log/beam3/pipeline1.log &
 pidp1=$!
 sleep 5
-numactl -C 12-17 ABServer --config=/home/artemis/Survey/Config/Beam2_server.xml &> /data/Survey/Log/server0.log &
+cd /data/Survey/Log/beam2
+numactl -C 12-17 ABServer --config=/home/artemis/Survey/Config/Beam2_server.xml &> /data/Survey/Log/beam2/server0.log &
 pids0=$!
-numactl -C 18-23 ABServer --config=/home/artemis/Survey/Config/Beam3_server.xml &> /data/Survey/Log/server1.log &
+cd /data/Survey/Log/beam3
+numactl -C 18-23 ABServer --config=/home/artemis/Survey/Config/Beam3_server.xml &> /data/Survey/Log/beam3/server1.log &
 pids1=$!
+echo $pidp0 $pidp1 $pids0 $pids1
 wait $pidp0
 wait $pidp1
 wait $pids0
--- Scripts/frb_abc3.sh
+++ Scripts/frb_abc3.sh
@@ -3,10 +3,13 @@ mkdir /data/Survey/Data
 /home/artemis/Survey/Scripts/killobs
 /home/artemis/Survey/Scripts/killobs
 /home/artemis/Survey/Scripts/killobs
-numactl -C 0-5 -l ABPipeline --config=/home/artemis/Survey/Config/Beam6_client.xml &> /data/Survey/Log/pipeline0.log &
+cd /data/Survey/Log/beam6
+numactl -C 0-5 -l ABPipeline --config=/home/artemis/Survey/Config/Beam6_client.xml &> /data/Survey/Log/beam6/pipeline0.log &
 pidp0=$!
 sleep 5
-numactl -C 12-17 ABServer --config=/home/artemis/Survey/Config/Beam6_server.xml &> /data/Survey/Log/server0.log &
+cd /data/Survey/Log/beam6
+numactl -C 12-17 ABServer --config=/home/artemis/Survey/Config/Beam6_server.xml &> /data/Survey/Log/beam6/server0.log &
 pids0=$!
+echo $pidp0 $pids0
 wait $pidp0
 wait $pids0
