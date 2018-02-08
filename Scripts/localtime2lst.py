6a10bf1acc054ba6826bfa140d4e2b16f43c72b8
ALFABURST artemis <griffin.foster@gmail.com>
2018-02-08 11:56:35 -0800

    updated to use pytz

--- Scripts/localtime2lst.py
+++ Scripts/localtime2lst.py
@@ -3,7 +3,8 @@
 import sys
 import ephem
 from datetime import datetime
-from dateutil import tz
+#from dateutil import tz
+import pytz
 
 # read local time; should be of the form "YYYY-mm-dd HH:MM:SS"
 localTime = sys.argv[1]
@@ -14,8 +15,12 @@ arecibo.lon = "-66:45:11.1"
 
 # pyEphem requires time to be in UTC (even at observer's location!)
 tempTime = datetime.strptime(localTime, "%Y-%m-%d %H:%M:%S")
-tempTime = tempTime.replace(tzinfo=tz.gettz("AST"))
-universalTime = tempTime.astimezone(tz.gettz("UTC"))
+#tempTime = tempTime.replace(tzinfo=tz.gettz("AST"))
+tempTime = tempTime.replace(tzinfo=pytz.timezone("America/Puerto_Rico"))
+
+utc = pytz.timezone('UTC')
+universalTime = tempTime.astimezone(utc)
+#universalTime = tempTime.astimezone(tz.gettz("UTC"))
 
 # set observer's time
 arecibo.date = universalTime
